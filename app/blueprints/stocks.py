100d6ecce9b04dcf93a5f05fea90ed20492eafe3
Shi-Hiers <shidericahiers03@gmail.com>
2024-11-04 13:59:02 -0500

    .

--- app/blueprints/stocks.py
+++ app/blueprints/stocks.py
@@ -6,9 +6,15 @@ stocks_bp = Blueprint('stocks', __name__)
 @stocks_bp.route('/stocks', methods=['GET', 'POST'])
 def get_stock_price():
     stock_data = None
+    ticker = None
+
     if request.method == 'POST':
         ticker = request.form.get('ticker')
         if ticker:
             stock = yf.Ticker(ticker)
             stock_data = stock.history(period="1d")
-    return render_template('stocks.html', stock_data=stock_data)
+            if not stock_data.empty:
+                stock_data.reset_index(inplace=True)
+                stock_data = stock_data.to_dict('records')
+
+    return render_template('stocks.html', stock_data=stock_data, ticker=ticker)
